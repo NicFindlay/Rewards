5ae14e203c2f7d170408cb62011d9284755a455e
Nic Findlay <info@nicfindlay.com>
2017-04-25 09:20:42 +0200

    Added User Profile andGlance Model

--- Glancer/models.py
+++ Glancer/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+#User profile
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+
+#Profile automatically created/updated when we create/update User instances
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+class Glance(models.Model):
+    value = 1
+    date = models.DateTimeField('Date Sent')
+    description = models.TextField(max_length=200, blank=True)
+    sender = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    receiver = models.ForeignKey(Profile, on_delete=models.CASCADE)
