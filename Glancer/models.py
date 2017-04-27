f692a5582ac176b3584e71be56ebeac887a92d3f
Nic Findlay <info@nicfindlay.com>
2017-04-27 20:33:57 +0200

    Colour changes and profile update save methods working (incorrect indent)

--- Glancer/models.py
+++ Glancer/models.py
@@ -15,15 +15,15 @@ class Profile(models.Model):
     def __str__(self):
         return str(self.user.first_name + " " + self.user.last_name)
 
-#Profile automatically created/updated when we create/update User instances
-@receiver(post_save, sender=User)
-def create_user_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+    #Profile automatically created/updated when we create/update User instances
+    @receiver(post_save, sender=User)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
 
-@receiver(post_save, sender=User)
-def save_user_profile(sender, instance, **kwargs):
-    instance.profile.save()
+    @receiver(post_save, sender=User)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.profile.save()
 
 #Glance model. Defines Glance (Point) class
 class Glance(models.Model):
